b59a726fdc75791681a45ae19f2cdfb8b08aab52
zhangsongcui3371@sina.com <zhangsongcui3371@sina.com@05073e99-9a1f-6a13-9a97-5e301057c82e>
2014-04-09 12:36:19 +0000

    Sending attachment finished, ChatInput improved

--- trunk/Controllers/ConversationController.ts
+++ trunk/Controllers/ConversationController.ts
@@ -11,6 +11,8 @@ module Controllers {
         convForm: ng.IFormController;
 
         FileSelected($files: Array<File>): void;
+        KeyDown($event: JQueryKeyEventObject): void;
+        KeyPress($event: JQueryKeyEventObject): void;
         Send(): void;
     }
 
@@ -36,35 +38,43 @@ module Controllers {
             $scope.Attachment= $files[0];
         };
 
+        $scope.KeyDown = $event=> {
+            if ($event.keyCode == 27 /* ESC */) {
+                $scope.Attachment = null;
+                $event.preventDefault();
+            }
+        };
+
+        $scope.KeyPress = $event=> {
+            if ($event.keyCode == 13 /* ENTER */ && !$event.shiftKey) {
+                if ($scope.convForm.$valid) {
+                    $scope.Send();
+                }
+                $event.preventDefault();
+            }
+        };
+
         $scope.Send = () => {
             // Code used: http://stackoverflow.com/questions/105034/how-to-create-a-guid-uuid-in-javascript
             var uuid = 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, c=> {
                 var r = Math.random() * 16 | 0, v = c == 'x' ? r : (r & 0x3 | 0x8);
                 return v.toString(16);
             });
-            $http.post(Managers.Constants.RelayUrl + "/text_conversations/" + convId + "/text_messages", {
-                text_message: {
-                    client_uuid: uuid,
-                    content: $scope.ChatInput
-                }
-            }).success((data: Models.IJsTextMessage) => {
+
+            $upload.upload({
+                url: Managers.Constants.RelayUrl + "/text_conversations/" + convId + "/text_messages",
+                data: {
+                    "text_message[client_uuid]": uuid,
+                    "text_message[content]": $scope.ChatInput
+                },
+                fileFormDataName: "text_message[attachment]",
+                file: $scope.Attachment
+            }).progress((evt: ProgressEvent)=> {
+                console.log('percent: ' + (100.0 * evt.loaded / evt.total));
+            }).success((data: Models.IJsTextMessage)=> {
+                // file is uploaded successfully
                 $scope.Messages.push(new Models.TextMessage(data));
             });
-            //$upload.upload({
-            //    url: Managers.Constants.RelayUrl + "/text_conversations/" + convId + "/text_messages",
-            //    data: {
-            //        text_message: {
-            //            client_uuid: uuid,
-            //            content: $scope.ChatInput
-            //        },
-            //    },
-            //    file: $scope.Attachment
-            //}).progress((evt: ProgressEvent)=> {
-            //    console.log('percent: ' + (100.0 * evt.loaded / evt.total));
-            //}).success((data: Models.IJsTextMessage)=> {
-            //    // file is uploaded successfully
-            //    $scope.Messages.push(new Models.TextMessage(data));
-            //});
             $scope.ChatInput = "";
             $scope.Attachment = null;
         };
