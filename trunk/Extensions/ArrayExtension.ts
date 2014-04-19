60a1c0dfbf8c16ba0ae61c72922c3c42b7745088
zhangsongcui3371@sina.com <zhangsongcui3371@sina.com@05073e99-9a1f-6a13-9a97-5e301057c82e>
2014-04-19 03:42:25 +0000

    Load message history

---
+++ trunk/Extensions/ArrayExtension.ts
@@ -0,0 +1,27 @@
+﻿interface Array<T> {
+    first(): T;
+    first(callbackfn: (value: T, index: number, array: T[]) => boolean): T;
+
+    findFirstIndex(callbackfn: (value: T, index: number, array: T[]) => boolean): number;
+}
+
+Array.prototype.first = (): any=> this[0];
+
+(<any>Array.prototype).first = (callbackfn: (value: any, index: number, array: any[]) => boolean): any=> {
+    for (var i = 0; i < this.length; ++i) {
+        var value = this[i];
+        if (callbackfn(value, i, this)) {
+            return value;
+        }
+    }
+    return undefined;
+};
+
+(<any>Array.prototype).findFirstIndex = (callbackfn: (value: any, index: number, array: any[]) => boolean): any=> {
+    for (var i = 0; i < this.length; ++i) {
+        if (callbackfn(this[i], i, this)) {
+            return i;
+        }
+    }
+    return -1;
+};
